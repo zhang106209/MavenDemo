6fca99b2d6730a2e638ec14b0c8fe5ab4bb6411b
weizhang39 <weizhang39@iflytek.com>
2020-08-20 14:00:43 +0800

    将目录删除

--- app/src/main/java/com/zw/mavendemo/MainActivity.java
+++ app/src/main/java/com/zw/mavendemo/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.zw.baselib.TestActivity;
+//import com.zw.baselib.TestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, TestActivity.class);
-                startActivity(intent);
+//                Intent intent=new Intent(MainActivity.this, TestActivity.class);
+//                startActivity(intent);
             }
         });
     }
